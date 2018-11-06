4acbecc6ce5b5f5fd780e3ffe956a43b6fca4a48
brianhjelle <bhjelle@g.ucla.edu>
2018-11-06 11:57:20 -0500

    Initializing rows and cols

--- addon/components/ember-kitchen-table.js
+++ addon/components/ember-kitchen-table.js
@@ -1,6 +1,14 @@
 import Component from '@ember/component';
 import layout from '../templates/components/ember-kitchen-table';
+import { service } from '@ember-decorators/service';
 
 export default class EmberKitchenTableComponent extends Component {
   layout = layout;
+
+  @service tableData
+
+  didReceiveAttrs() {
+    super.didReceiveAttrs(...arguments);
+    this.tableData.setData(this.data, this.cols);
+  }
 }
